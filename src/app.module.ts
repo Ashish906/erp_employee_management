4c5eef6dc39e116d3c27bcaeb8352257b84c8cd7
Ashish906 <ashish.cse.gu@gmail.com>
2024-11-08 02:51:18 +0600

    Implement employee hierarchy storing functionality.

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {ConfigModule} from "@nestjs/config";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
+import { EmployeesModule } from './employees/employees.module';
+import { PositionsModule } from './positions/positions.module';
+
+const envPath = join(__dirname,'..', process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env').trim();
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: envPath,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      sync: {
+        alter: true
+      },
+      synchronize: true,
+      autoLoadModels: true,
+    }),
+    EmployeesModule,
+    PositionsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
