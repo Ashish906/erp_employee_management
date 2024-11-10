8acd10f17a91023734e648dc6c5c5abdf1252219
Ashish906 <ashish.cse.gu@gmail.com>
2024-11-10 21:23:37 +0600

    Fixed query format issue.

--- src/common/dto/common-query.dto.ts
+++ src/common/dto/common-query.dto.ts
@@ -1,11 +1,14 @@
 import { IsInt, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CommonQueryDto {
   @IsInt()
+  @Type(() => Number)
   page: number;
 
   @Max(50)
   @Min(1)
   @IsInt()
+  @Type(() => Number)
   size: number;
 }
